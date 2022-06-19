a486f16aa6cb0ad8a93eff45137e38e8a02722fa
yoshino9397 <yosino8512@gmail.com>
2022-06-19 00:48:22 -0700

    navbar added download part

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,18 +1,42 @@
 import "./navbar.scss";
 import { Link } from "react-router-dom";
+import DownloadIcon from "@mui/icons-material/Download";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = (e) => {
+    if (e.target === e.currentTarget) {
+      setOpen(false);
+    } else {
+      setOpen(!open);
+    }
+  };
+
   return (
-    <div className="navbar">
-      <div className="wrapper">
-        <div className="top">
-          <h3>Reports</h3>
-          <Link to="/" style={{ textDecoration: "none" }}>
-            <h4>Summary</h4>
-          </Link>
+    <>
+      <div className="whole" onClick={handleOpen}></div>
+      <div className="navbar">
+        <div className="wrapper">
+          <div className="top">
+            <h3>Reports</h3>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <h4>Summary</h4>
+            </Link>
+          </div>
+          <div className="download" onClick={() => setOpen(!open)}>
+            <DownloadIcon />
+            {open && (
+              <div className="downloadList">
+                <p>Download PDF</p>
+                <p>Download CSV</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
